3f993ebac0ce24ca1d541391eff26e8d773d4968
EstivenCano <estivencano99@gmail.com>
2021-09-16 14:19:58 -0500

    Rutas básicas usuario

---
+++ routes/usuarios.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const Usuario = require("../models/usuario");
+const passport = require("passport");
+const autenticacion = require("../services/autenticacion");
+
+const cors = require("../services/cors");
+const usuarioRouter = express.Router();
+
+// Obtiene todos los usuarios
+usuarioRouter.get(
+  "/",
+  cors.corsWithOptions,
+  autenticacion.verifyUser,
+  autenticacion.verifyAdmin,
+  (req, res, next) => {
+    Usuario.find({}, (err, usuarios) => {
+      if (err) {
+        return next(err);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json(usuarios);
+      }
+    });
+  }
+);
+
+// Ruta para crear un usuario
+usuarioRouter.post("/signup", cors.corsWithOptions, (req, res, next) => {
+  //Registra un usuario
+  Usuario.register(
+    new Usuario({ correo: req.body.correo }),
+    req.body.contraseña,
+    (err, usuario) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({ error: err });
+        return;
+      } else {
+        if (req.body.nombres) usuario.nombres = req.body.nombres;
+        if (req.body.apellidos) usuario.apellidos = req.body.apellidos;
+        usuarios.save((err, usuario) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ error: err });
+            return;
+          }
+          passport.authenticate("local")(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json({ success: true, status: "Registro satisfactorio" });
+          });
+        });
+      }
+    }
+  );
+});
+
+// Ruta para el inicio de sesión de un usuario
+usuarioRouter.post(
+  "/login",
+  passport.authenticate("local"),
+  cors.corsWithOptions,
+  (req, res) => {
+    const token = autenticacion.getToken({ _id: req.user_id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "Has iniciado sesión correctamente",
+    });
+  }
+);
+
+// Ruta para cerrar sesión
+usuarioRouter.get("/logout", cors.corsWithOptions, (req, res, next) => {
+  if (req.sesion) {
+    req.session.destroy();
+    res.clearCookie("session-id");
+    res.redirect("/");
+  } else {
+    const err = new Error("Parece que no estás logeado");
+    err.status = 403;
+    next(err);
+  }
+});
+
+module.exports = usuarioRouter;
