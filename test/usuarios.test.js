acf73b397b8b2055f3be820aee0239a78243d521
Estiven Cano <estivencano99@gmail.com>
2021-11-02 13:14:44 -0500

    [Fix] Documentación de pruebas

--- test/usuarios.test.js
+++ test/usuarios.test.js
@@ -112,8 +112,10 @@ describe("Obtener usuarios", () => {
         .set("Authorization", `Bearer ${token}`)
         .expect(200)
         .expect("Content-Type", /application\/json/);
-      // Asigna el ID del último usuario para realizar las pruebas posteriores
+
+      // Asigna el ID del primer usuario
       idUsuario = res.body[0]._id;
+      // Asigna el ID del último usuario para realizar delete
       idUsuario2 = res.body[res.body.length - 1]._id;
       expect(Array.isArray(res.body)).toBeTruthy();
     });
